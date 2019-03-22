d5b81b16a2062928aa41c1b19e2ae8e65f696e29
RyanGowan <rygowan@gmail.com>
2019-03-22 11:15:21 -0300

    Update Mainclass.java

--- core/src/com/groupproject/game/Mainclass.java
+++ core/src/com/groupproject/game/Mainclass.java
@@ -2,31 +2,49 @@ package com.groupproject.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.groupproject.util.GameClientService;
 
 public class Mainclass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	GameClientService gcs;
-	
+	private Sprite sprite;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		gcs = new GameClientService();
-		gcs.sendRequest("My potato flow around... round and round around!");
+		sprite = new Sprite(img);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+		{
+			sprite.translateX(-10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP))
+		{
+			sprite.translateY(10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+		{
+			sprite.translateX(10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+		{
+			sprite.translateY(-10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+		{
+			//shoot bullet
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 	
