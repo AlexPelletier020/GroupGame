baf2479b0c0a1f7de9154a0f77fe80458ddad15b
RyanGowan <rygowan@gmail.com>
2019-03-29 16:07:44 -0300

    added new map
    new map is implemented

--- core/src/com/groupproject/util/FileUtilities.java
+++ core/src/com/groupproject/util/FileUtilities.java
@@ -11,36 +11,40 @@ public class FileUtilities {
 
 	public static char[][] readLeaderBoardFile(String filePath) {
 		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-		// Method				:	public ArrayList<char[][]> readLeaderBoardFile(String filePath)
+		// Method : public ArrayList<char[][]> readLeaderBoardFile(String filePath)
 		//
-		// Method parameters	:	filePath - the path to the puzzles
+		// Method parameters : filePath - the path to the puzzles
 		//
-		// Method return		:	ArrayList<char[][]> - a list of puzzles
+		// Method return : ArrayList<char[][]> - a list of puzzles
 		//
-		// Synopsis				:   Read The leader board file and copy the lines in an array list of char[][].
-		//							
+		// Synopsis : Read The leader board file and copy the lines in an array list of
+		// char[][].
 		//
-		// Modifications		:
-		//								   Date						  Developer								Notes
-		//									----			      			---------			 	     			 -----
-		//							Sep18th, 2018		  Mohammed Al-Safwan				Initial setup
+		//
+		// Modifications :
+		// Date Developer Notes
+		// ---- --------- -----
+		// Sep18th, 2018 Mohammed Al-Safwan Initial setup
 		//
 		// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-		char[][] cells = new char[20][20];
+		char[][] cells = new char[37][21];
 
-		FileHandle handle = Gdx.files.internal(filePath); //initialize BufferReader
-		String line = ""; //initialize an empty string to hold each line
+		FileHandle handle = Gdx.files.internal(filePath); // initialize BufferReader
 
-		if (null != (line = handle.readString())) { //Read a line and check if it's not null
+		String line = ""; // initialize an empty string to hold each line
 
-			String[] cell = line.split("\\r?\\n"); //Split the line using "," to an array
+		if (null != (line = handle.readString())) { // Read a line and check if it's not null
+
+			String[] cell = line.split("\\r?\\n"); // Split the line using "," to an array
 			String[] completeLine;
 			for (int height = cell.length - 1; height >= 0; height--) {
 
 				completeLine = (cell[height].split(" "));
 
-				for (int width = cell.length - 1; width >= 0; width--) { //Add the chars to the cells
+				for (int width = completeLine.length - 1; width >= 0; width--) { // Add the chars to the cells
+					System.out.println(width);
+					System.out.println(height);
 					cells[height][width] = completeLine[width].charAt(0);
 					System.out.print(cells[height][width] + " ");
 				}
@@ -49,6 +53,6 @@ public class FileUtilities {
 			}
 		}
 
-		return cells; //return the ArrayList of the puzzles
+		return cells; // return the ArrayList of the puzzles
 	}
 }
